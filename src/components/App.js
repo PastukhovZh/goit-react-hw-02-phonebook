6f34e25914be72ffb59544b42ff784649d7d8eb3
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-09-29 23:48:43 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
-export const App = () => {
-  return (
-    <div>
-      
+import { Component } from "react"
+
+export class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+}
+  
+  render() {
+    return <div>
+<input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
     </div>
-  );
+  }
 };
